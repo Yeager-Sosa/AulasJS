cc0780f4a3050c42cbd4d4d6ef9cb31099bd0a65
Gustavo Sosa <dante-sosa@hotmail.com>
2021-04-29 10:41:25 -0300

    arrumando identação

--- gustavo-sosa-aulas-js-primeiros-passos-d05b92a625f7/js_logico/aula46_intervalTimeout/index.js
+++ gustavo-sosa-aulas-js-primeiros-passos-d05b92a625f7/js_logico/aula46_intervalTimeout/index.js
@@ -1,8 +1,8 @@
-function mostraHora(){//função que mostra hora atual
+function mostraHora() {//função que mostra hora atual
     let data = new Date();
 
     return data.toLocaleTimeString('pt-BR', {
-    hour12:false
+        hour12: false
     });
 }
 
@@ -10,14 +10,14 @@ console.log(mostraHora());
 
 
 
-const timer = setInterval(function funcaoDoInterval(){//função que faz alguma coisa com um intervalo de determinado ms
+const timer = setInterval(function funcaoDoInterval() {//função que faz alguma coisa com um intervalo de determinado ms
     console.log(mostraHora());
 }, 1000);
 
-setTimeout(function(){ //função que faz algo uma unica vez e para em determinado ms
-//clearInterval(timer);
+setTimeout(function () { //função que faz algo uma unica vez e para em determinado ms
+    //clearInterval(timer);
 }, 3000);
 
-setTimeout(function(){
-console.log("ola mundo");
-},5000);
+setTimeout(function () {
+    console.log("ola mundo");
+}, 5000);
